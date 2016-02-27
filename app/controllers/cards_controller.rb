00b5e062a1175d885ce1b7513827f6c017103694
nikfor <nikfor1@gmail.com>
2016-02-27 12:53:44 +0300

    third-task first attempt
    third-task second attempt
    
    third-task third-commit

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,48 @@
 class CardsController < ApplicationController
+  
+  before_action :find_card, only: [:edit, :update, :destroy]
+
   def index
     @cards = Card.all
+  end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.create(card_params)
+    if @card.valid?
+      redirect_to cards_path
+    else
+      render "new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @card.update_attributes(card_params)
+    if @card.valid?
+      redirect_to cards_path
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @card.destroy
+    redirect_to cards_path
+  end
+
+  private
+
+  def card_params
+    params.require(:card).permit(:original_text, :translated_text)
+  end
+
+  def find_card
+    @card = Card.find(params[:id])
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,16 @@
 class Card < ActiveRecord::Base
-  #attr_accessible :original_text, :translated_text, :review_date
-  validates :original_text, :translated_text, presence: true
+
+  before_validation :card_date_set, on: :create
+  validates :review_date, :translated_text, :original_text, presence: true
+  validate :translate_not_eql_orginal
+
+  def translate_not_eql_orginal
+    errors.add(:original_text, I18n.t("errors.eql_validate_err")) if
+      translated_text.downcase == original_text.downcase
+  end
+
+  def card_date_set
+    self.review_date ||= Date.today + 3
+  end
+
 end
