667512a17472129911ec02d3b56d44a2d41c4052
nikfor <nikfor1@gmail.com>
2016-06-28 02:22:51 +0300

    some changes for work with twitter auth

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -10,7 +10,7 @@ Rails.application.config.sorcery.configure do |config|
   config.external_providers = [:twitter]
   config.twitter.key = ENV['TWITTER_KEY']
   config.twitter.secret = ENV['TWITTER_SECRET']
-  config.twitter.callback_url = "http://localhost:3000/oauth/callback?provider=twitter"
+  config.twitter.callback_url = ENV['TWITTER_CALLBACK_URL']
   config.twitter.user_info_mapping = {:email=> "screen_name"}
 
   # -- core --
